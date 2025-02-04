d2b9359fcffce512f827c55054988954c85c2a50
ahmeddkhann <iamahmedkhan02@gmail.com>
2025-02-04 10:48:04 +0500

    optimized code in comparitively less files

--- admin_operations.py
+++ admin_operations.py
@@ -1,5 +1,28 @@
 from authentication import check_auth
 from connect import create_collection
+from requirements import requirements
+
+
+@check_auth
+def add_admin():
+    try:   
+        user_details = requirements()
+        if len(user_details) == 4:
+              name, username, password, role = user_details
+
+        collection = create_collection("admin")
+        admin_data = {
+              "name": name,
+              "username": username,
+              "password": password,
+              "role": role
+       }
+        collection.insert_one(admin_data)
+        print("Admin added successfully")
+
+    except Exception as e:
+         print(f"Error while adding user: {e}")
+
 
 @check_auth       
 def remove_admin():
@@ -18,3 +41,8 @@ def remove_admin():
           print(f"Error ocurred while removing an admin {e}")
 
 remove_admin()
+              
+
+
+
+        
